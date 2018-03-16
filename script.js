6a04303cd11263c9855e0eb86d15fb65a0461cb1
yosabel tekeste <yosabeltekeste@students.berkeley.net>
2018-03-16 11:35:33 -0700

    hi

--- script.js
+++ script.js
@@ -1,21 +1,18 @@
 $(document).ready(function() {
     $('#buttongo').on('click', function(){
     var artist=$('#artist').val();
-    var res=$('#results').val();
+    var res=$('#result').val();
+    console.log(res);
         $.ajax({
             url: "https://itunes.apple.com/search?term="+ artist + "&limit=" + res,
             type: 'GET',
             crossDomain: true,
             dataType: 'jsonp',
             success: function(result) {
-                console.log(result);
                 myFunction(result);
             },
             error: function() { alert('Failed!'); }
         });
-
-
-
 
 
     });
@@ -24,14 +21,23 @@ $(document).ready(function() {
 });
 
 
-function myFunction(json){
-    var list="<table border='1'>";
-    for(var i=0;json.results.length;i++){
+function myFunction(data){
+    console.log(data);
+
+    var list="<table border= '1'>";
+    for(var i=0;i<data.results.length;i++){
         list+="<tr>";
-        // list+="<td>" + json.results[i]. + "</td>";
-        list+="<td>" + json.results[i].collectionName + "</td>";
-        // list+="<td>" + json.results[i]. + "</td>";
-        list+="</tr>";
+        list+="<td>" + data.results[i].artistName + "</td>";
+        list+="<td>" + "<img src='" + data.results[i].artworkUrl100 + "'>" + "</td>";
+        list+="<td>" + data.results[i].collectionName + "</td>";
+        list+="<td>" + data.results[i].trackName + "</td>";
+        list+="<td>" + data.results[i].collectionPrice + "</td>";
+        list+="<td>" + data.results[i].currency + "</td>";
+        // list+="<td>" + '<audio controls="true" src= + "</td>";
+        list+="<td>" + data.results[i].collectionExplicitness + "</td>";
+
+
+            list+="</tr>";
     }
     list+="</table>";
 $('#list').html(list);
