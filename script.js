a93742a7ae04875b9f58c531fa82f064aa8bf495
yosabel tekeste <yosabeltekeste@students.berkeley.net>
2018-03-21 16:35:54 -0700

    hi

--- script.js
+++ script.js
@@ -21,21 +21,37 @@ $(document).ready(function() {
 });
 
 
+
 function myFunction(data){
     console.log(data);
-
+//how to replace + with _
     var list="<table border= '1'>";
+    list+="<td>" + 'Artist(s)' + "</td>";
+    list+="<td>" + 'Wikipdia link of Artist' + "</td>";
+    list+="<td>" + 'Genre' + "</td>";
+    list+="<td>" + 'Album Cover' + "</td>";
+    list+="<td>" + 'Album' + "</td>";
+    list+="<td>" + 'Song' + "</td>";
+    list+="<td>" + 'Collection Price' + "</td>";
+    list+="<td>" + 'Currency' + "</td>";
+    list+="<td>" + 'Rap Genius suggestions' + "</td>";
+    list+="<td>" + 'Preview' + "</td>";
+    list+="<td>" + 'Explicitness ranking' + "</td>";
     for(var i=0;i<data.results.length;i++){
+        var artist=data.results[i].artistName;
         list+="<tr>";
+
         list+="<td>" + data.results[i].artistName + "</td>";
-        list+="<td>" + "<img src='" + data.results[i].artworkUrl100 + "'>" + "</td>";
-        list+="<td>" + data.results[i].collectionName + "</td>";
+        list+="<td><a href ='https://en.wikipedia.org/wiki/" + artist + "'target='_blank' > All about  " +  artist + "</a></td>";
+        list+="<td>" + data.results[i].primaryGenreName + "</td>";
+        list+="<td><img src='" + data.results[i].artworkUrl100 + "'></td>";
+        list+="<td><a href ='" + data.results[i].collectionViewUrl + "' target='_blank'>" + data.results[i].collectionName + "</a></td>";
         list+="<td>" + data.results[i].trackName + "</td>";
         list+="<td>" + data.results[i].collectionPrice + "</td>";
         list+="<td>" + data.results[i].currency + "</td>";
-        // list+="<td>" + '<audio controls="true" src= + "</td>";
+        list+="<td><a href ='https://genius.com/artists/" + artist + "'target='_blank' > Suggestions of  " +  artist + "</a></td>";
+        list+="<td>" + "<audio controls='true'  src='" + data.results[i].previewUrl+ "' id= 'audio' type='audio/n4a'></audio>" + "</td>";
         list+="<td>" + data.results[i].collectionExplicitness + "</td>";
-
 
             list+="</tr>";
     }
